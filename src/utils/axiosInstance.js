14b0a32ec4fe6586a33017057f5ba56c284b452c
Omkar <omkarghodekar140803@gmail.com>
2025-08-15 19:08:55 +0530

    debugging

--- src/utils/axiosInstance.js
+++ src/utils/axiosInstance.js
@@ -1,7 +1,7 @@
 import axios from "axios"
 
 const axiosInstance = axios.create({
-    baseURL: import.meta.env.VITE_BACKEND_BASE_URL,
+    baseURL: "https://shortify-o6vy.onrender.com",
     timeout:10000, //10s
     withCredentials:true
 })
